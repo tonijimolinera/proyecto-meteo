da11b792857c016b0aea261f3b7bad2f30cb89b2
tonijimolinera <118393405+tonijimolinera@users.noreply.github.com>
2022-11-16 23:27:01 +0100

    Update MeteoController.java

--- meteoajm/src/main/java/com/meteoajm/meteoajm/controllers/MeteoController.java
+++ meteoajm/src/main/java/com/meteoajm/meteoajm/controllers/MeteoController.java
@@ -40,7 +40,7 @@ public class MeteoController {
 	@DeleteMapping(value = "/station/{id}")
 	public String deleteMeteoStationById(@PathVariable ("id") Integer id){
 		meteoStationService.deleteMeteoStations(id);
-		return "Elemento borrado 333";
+		return "Elemento borrado 444";
 	}
 	
 	@DeleteMapping(value = "/stationsDeleteByName/{name}")
